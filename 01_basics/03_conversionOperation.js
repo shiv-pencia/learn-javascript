78b3f59fde4d95fb7465e2b6e29ab0cf3078a87a
shiv-pencia <shivprakashpencia@gmail.com>
2024-03-26 20:56:36 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = Boolean
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+
+//console.log( typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// 1 => true ; 0 => false 
+// ""=> false 
+//"shiv" => true 
+
