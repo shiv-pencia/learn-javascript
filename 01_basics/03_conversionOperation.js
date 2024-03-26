e2abbd64239cf49e49c63f9f75f1d28002c56def
shiv-pencia <shivprakashpencia@gmail.com>
2024-03-26 21:11:45 +0530

    ..

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,3 +13,17 @@ let score = Boolean
 // ""=> false 
 //"shiv" => true 
 
+//************ operations *************
+let value  = 3 
+let negValue = -value 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+
+let str1 = "shiv "
+let str2 = "pencia"
+let str3 = str1 + str2
+
+console.log(str3);
+
