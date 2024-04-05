5abe960d5cc4adaa8f10f3fb4af88185ad6183d9
shiv-pencia <shivprakashpencia@gmail.com>
2024-04-05 08:50:53 +0530

    If Else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,81 @@
+//if
+
+const isUserloggedIn = true;
+const temperature = 61;
+
+// if (temperature < 50) {
+//   console.log("less than 50");
+// } else {
+//   console.log("NO");
+// }
+
+const score = 200;
+
+if (score > 100) {
+  const power = "fly";
+  // console.log(`User power: ${power}`);
+}
+//console.log(`User power: ${power}`); output  power not defined (scope error)
+
+const balance = 800;
+// if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("less than 900");
+// } else {
+//   console.log("less than 1200");
+// }
+
+const userLoggedin = true 
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedin && debitCard) {
+    console.log("Allow to buy course");
+    
+}
+
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User loggeg in");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
