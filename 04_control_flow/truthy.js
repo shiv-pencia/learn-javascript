97fac59a33bccfdbbb86ee935f5dcc80d212f611
shiv-pencia <shivprakashpencia@gmail.com>
2024-04-05 12:01:03 +0530

    terniary operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,16 +1,14 @@
-const userEmail = []
+const userEmail = [];
 
 if (userEmail) {
-    console.log("Got user email");
-}else {
-    console.log("Don't have user email");
+//   console.log("Got user email");
+} else {
+  console.log("Don't have user email");
 }
-
 
 //---------------falsy values--------
 
-// false , 0 ,-0,bigInt 0n , "",null ,undefined ,NaN 
-
+// false , 0 ,-0,bigInt 0n , "",null ,undefined ,NaN
 
 //---------------truthy values -------------
 
@@ -20,8 +18,31 @@ if (userEmail) {
 //     console.log("Array is empty");
 // }
 
-const emptyObj = {}
+const emptyObj = {};
 if (Object.keys(emptyObj).length === 0) {
-    console.log("empty");
-    
+//   console.log("empty");
 }
+
+// Nulish coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+
+// val1 = null ?? 10
+
+// val1 = undefined ?? 15
+
+val1 = null ?? 10 ?? 15
+
+// console.log(val1);
+
+// terniary Operator
+
+// condition ? true : false 
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+
+
