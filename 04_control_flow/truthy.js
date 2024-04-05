40abf86cc05892b9d60355f2db39eb860cf214c1
shiv-pencia <shivprakashpencia@gmail.com>
2024-04-05 11:48:23 +0530

    ------

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,1 +1,27 @@
-const userEmail = "shiv.ai"
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+}else {
+    console.log("Don't have user email");
+}
+
+
+//---------------falsy values--------
+
+// false , 0 ,-0,bigInt 0n , "",null ,undefined ,NaN 
+
+
+//---------------truthy values -------------
+
+// "0" ,  'false' , "  " , [] , {} , function () {},
+
+// if ( userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+if (Object.keys(emptyObj).length === 0) {
+    console.log("empty");
+    
+}
