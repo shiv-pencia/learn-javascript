945c4bfaa763fd3aac67bb55fc2ae3aaccff1680
shiv-pencia <shivprakashpencia@gmail.com>
2024-04-06 09:30:08 +0530

    while and do while loop

---
+++ 05_loops/two.js
@@ -0,0 +1,32 @@
+//--------------while and do while loop----------
+let index = 0
+//  while (index <= 10) {
+//      console.log(`Value of index is ${index}`);
+
+//      index  = index +2
+    
+//  }
+
+ let meArray = ["flash", 'batman', "superman"]
+ let arr = 0
+//  while (arr < meArray.length) {
+
+//     console.log(`Value is ${meArray[arr]}`)
+
+//     arr =  arr+1
+    
+//  }
+
+ let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+            
+
+
+
+
+
+
+
