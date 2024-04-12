37cc92dc026166dfdf7126ef84d8791784b5f244
shiv-pencia <shivprakashpencia@gmail.com>
2024-04-12 17:22:13 +0530

    project

---
+++ 07_projects/6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,26 @@
+const randomcolor = function () {
+  let hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+// console.log(randomcolor());
+let intervalId;
+
+function startChangingColor() {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomcolor();
+  }
+}
+function stopChangingColor() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
