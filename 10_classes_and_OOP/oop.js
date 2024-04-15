30500e73c7207cfbe512e7a5493a8f3352341ad1
shiv-pencia <shivprakashpencia@gmail.com>
2024-04-15 18:23:36 +0530

    opp

--- 10_classes_and_OOP/oop.js
+++ 10_classes_and_OOP/oop.js
@@ -5,10 +5,24 @@ const user = {
 
     getUserDetails: function(){
         //console.log("Got user details from database");
-        console.log(`username is : ${this.username}`);
-        console.log(this);
+        // console.log(`username is : ${this.username}`);
+        // console.log(this);
     }
 }
 
-console.log(user["username"]);
-console.log(user.getUserDetails());
+// console.log(user["username"]);
+// console.log(user.getUserDetails());
+
+function User (username, loginCount, isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    return this
+
+}
+
+const userOne = new User('shiv', 8, true)
+const userTwo = new User('pencia', 80, true)
+console.log(userOne);
+console.log(userTwo);
